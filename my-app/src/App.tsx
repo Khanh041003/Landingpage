7c662abc1cf4d6e673556c14e1cf5687e2fed1fb
mtuonglatui <tuongphan2004@gmail.com>
2025-02-07 12:55:14 +0700

    delete

--- my-app/src/App.tsx
+++
@@ -1,7 +0,0 @@
-export default function App() {
-  return (
-    <div className="bg-red-500 text-white p-6 text-center">
-      <h1 className="text-3xl font-bold">Hello, Tailwind CSS!</h1>
-    </div>
-  );
-}
